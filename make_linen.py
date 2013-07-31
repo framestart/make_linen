27a4ac09a243313ba3987a217a46562463ce9d59
Maciek Gryka <maciej.gryka@gmail.com>
2013-07-31 15:47:40 +0100

    more cleanup and some tiny optimizations

--- make_linen.py
+++ make_linen.py
@@ -9,8 +9,8 @@ def add_fiber(mask, fiber, pos, horizontal=True):
 	
 	Args:
 		mask: image to add to
-		fiber: fiber (1D array of intensitties)
-		pos: [x, y] position within the image
+		fiber: 1D array of intensitties
+		pos: (x, y) position within the image
 		horizontal: set to False for vertical fibers
 
 	"""
@@ -21,14 +21,14 @@ def add_fiber(mask, fiber, pos, horizontal=True):
 			mask[pos[1], pos[0]:pos[0] + fiber_len] += fiber
 		else:
 			diff = pos[0] + fiber_len - width
-			mask[pos[1], pos[0]:pos[0] + fiber_len - diff] += fiber[:-diff]
+			mask[pos[1], pos[0]:width] += fiber[:-diff]
 			mask[pos[1], 0:diff] += fiber[-diff:]
 	else:
 		if height >= pos[1] + fiber_len:
 			mask[pos[1]:pos[1] + fiber_len, pos[0]] += fiber
 		else:
 			diff = pos[1] + fiber_len - height
-			mask[pos[1]:pos[1] + fiber_len-diff, pos[0]] += fiber[:-diff]
+			mask[pos[1]:height, pos[0]] += fiber[:-diff]
 			mask[0:diff, pos[0]] += fiber[-diff:]
 
 
@@ -36,33 +36,37 @@ def add_fibers(mask, n_fibers=0, fiber_len=150, fiber_opacity=0.1,
 	horizontal=True, opacities={}):
 	"""Add the specified number of fibers into the given image."""
 	height, width = mask.shape
+	len_var = fiber_len / 5.0
 
-	for f in range(n_fibers):
+	for f in xrange(n_fibers):
 		# randomize fiber length slightly
-		fiber_len_r = int(fiber_len + randn()*fiber_len/5)
+		fiber_len_r = int(fiber_len + randn() * len_var)
 		
 		# initialize the fiber
 		fiber = np.ones(fiber_len_r)
 		
 		# modify the fiber so that it's strong (opaque) in the middle and 
 		# fades off towards the edges
-		if not opacities.has_key(fiber_len_r):
-			opacities[fiber_len_r] = np.ones(fiber_len_r)
-			for p in range(fiber_len_r):
-				hl = round(fiber_len_r/2)
-				opacities[fiber_len_r][p] = (hl - abs(hl - p))/hl
+		if fiber_len_r not in opacities:
+			# half length
+			hl = round(fiber_len_r/2)
+			# create opacity map of length fiber_len_r
+			opacities[fiber_len_r] = np.array([
+				(hl - abs(hl - p))/hl
+				for p in xrange(fiber_len_r)
+			])
 		# apply (slightly randomized) opacity
 		fiber = fiber * opacities[fiber_len_r] * fiber_opacity * rand()
 		
 		# randomize position
-		pos = [int(rand() * width), int(rand() * height)]
+		pos = (int(rand() * width), int(rand() * height))
 		
 		add_fiber(mask, fiber, pos, horizontal)
 
 	return mask
 
 
-def make_linen(fiber_len=50, fiber_opacity=0.01, n_fibers=50000,
+def make_linen(fiber_len=50, fiber_opacity=0.01, n_fibers=20000,
 	base_intensity=0.1):
 	"""
 	Create linen texture image and save as linen.png
@@ -74,13 +78,17 @@ def make_linen(fiber_len=50, fiber_opacity=0.01, n_fibers=50000,
 		base_intensity: base intensity of the image before adding any fibers
 
 	"""
-	im_size = (900, 1440)
+	im_size = (400, 400)
 
 	im = np.ones(im_size) * base_intensity
 
+	opacities = {}
+
 	mask = np.zeros(im_size)
-	mask = add_fibers(mask, n_fibers, fiber_len, fiber_opacity)
-	mask = add_fibers(mask, n_fibers, fiber_len, fiber_opacity, horizontal=False)
+	mask = add_fibers(mask, n_fibers, fiber_len, fiber_opacity,
+		opacities=opacities)
+	mask = add_fibers(mask, n_fibers, fiber_len, fiber_opacity,
+		opacities=opacities, horizontal=False)
 
 	im += mask
 
